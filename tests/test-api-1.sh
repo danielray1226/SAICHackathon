10ff7bd9774030b25886e251639facc10060fff9
Daniel <danielraybosh@gmail.com>
2024-11-11 22:35:00 -0600

    Made APIServlet and skeleton for future work

--- tests/test-api-1.sh
+++ tests/test-api-1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-REQUEST='{"hello" : "world", "daniel" : 2}'
-
+REQUEST=\
+`cat <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<note>
+  <to>Tove</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</note>
+EOF`
 echo "submitting : ${REQUEST}"
-echo ${REQUEST} | curl -i -v -X POST -d @- http://localhost:8000/TestAPI/
+echo ${REQUEST} | curl -i -v -X POST -d @- http://localhost:8000/hack/api
